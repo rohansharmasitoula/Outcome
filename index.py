e64a68a012167dbee76733658e1622dcd6e383fd
rohansharmasitoula <100085662+rohansharmasitoula@users.noreply.github.com>
2022-02-21 12:05:20 +0530

    Update index.py

--- index.py
+++ index.py
@@ -3,10 +3,13 @@ import pandas as pd
 import numpy as np
 from st_aggrid import AgGrid
 import matplotlib.pyplot as plt
-#import plotly.figure_factory as ff
-st.title('Programme Outcome using Normalization')
 
-st.write('Upload an excelsheet containing marks for a semester')
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+st.title('Calculating the Course Scores using Normalization')
+
+st.write('Upload an excelsheet containing subjects and the respective marks for a semester as given in the sample below')
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
@@ -17,17 +20,21 @@ if uploaded_file is not None:
     for column in df_min_max_scaled.columns:
     	df_min_max_scaled[column] = (df_min_max_scaled[column] - df_min_max_scaled[column].min()) / (df_min_max_scaled[column].max() - df_min_max_scaled[column].min())	
     AgGrid(df_min_max_scaled)
-    st.bar_chart(df_min_max_scaled)
+    for column in df_min_max_scaled.columns:
+        st.bar_chart(df_min_max_scaled[column])
     st.header('After Standardization')
     df_z_scaled = df.copy()
     for column in df_z_scaled.columns:
     	df_z_scaled[column] = (df_z_scaled[column] - df_z_scaled[column].mean()) / df_z_scaled[column].std()
     AgGrid(df_z_scaled)
-    st.bar_chart(df_z_scaled)
-    #fig = ff.create_distplot(df, bin_size=[.1, .25, .5])
-    #st.plotly_chart(fig, use_container_width=True)
+    for column in df_z_scaled.columns:
+        st.bar_chart(df_z_scaled[column])
+    st.header('Scores for all the subjects')
+    
+    st.write(df_min_max_scaled.describe())
 
-
-	
-
-
+    df_sc=df_min_max_scaled.describe()
+    
+    df_score=df_sc.iloc[1,:]
+    st.bar_chart(df_score)
+    
